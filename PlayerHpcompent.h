34f952e9cb27c87c0ff7018484378f3737ed1a1e
梁宸 <1657217487@qq.com>
2026-09-25 18:09:22 +0800

    增加了选择游戏

---
+++ PlayerHpcompent.h
@@ -0,0 +1,2 @@
+#pragma once
+float PlayerMaxHealth=100.0f;
--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <windows.h>
+#include "PlayerHpcompent.h"
+
 using namespace std;
 
 int main() 
 {
-	cout << "Hello, World!" << std::endl;
-	return 0;
+	SetConsoleOutputCP(CP_UTF8);
+	cout << "请输入数字选择进入的区域" << endl;
+	cout << "1. 房间 2. 森林 3. 沙漠" << endl;
+	int i = 0;
+	cin >> i;
+	switch (i)
+	{
+	case 1:
+	{
+		cout<<"你进入了房间。" << endl;
+		break;
+	}
+	case 2:
+	{
+		cout << "你进入了森林。" << endl;
+		break;
+	}
+	case 3:
+	{
+		cout << "你进入了沙漠。" << endl;
+		break;
+	}
+	default:
+		break;
+	}
 }
